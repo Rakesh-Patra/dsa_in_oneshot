d34006b11af88df1b35fe4cdadf3751d9f3d532e
Rakesh Ku Patra <rakeshkupatra3@gmail.com>
2026-02-26 00:09:01 +0530

    sort color or sort zero one two completed successfully

---
+++ array/sortZeroOneTwo.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sortZeroOneTwo(self, nums):
+        low=0
+        mid=0
+        high=len(nums)-1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                
+        print(nums)
+s=Solution()
+s.sortZeroOneTwo( [1, 0, 2, 1, 0])
+
+        
+        
+
+
+        
+       
