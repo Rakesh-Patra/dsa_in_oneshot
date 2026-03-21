20d2b0f285d42dd6766738fd90ca3ebdcaf06908
Rakesh Ku Patra <rakeshkupatra3@gmail.com>
2026-03-21 23:45:59 +0530

    min of subarray program completed successfully

---
+++ stack_queue/min_of_subarray2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def sumSubarrayMins(self,arr):
+
+         n = len(arr)
+         stack = []
+         left = [0] * n
+         right = [0] * n
+     
+         
+         for i in range(n):
+            count = 1
+            while stack and stack[-1][0] > arr[i]:
+                count += stack.pop()[1]
+            left[i] = count
+            stack.append((arr[i], count))
+     
+         stack = []
+     
+        
+         for i in range(n - 1, -1, -1):
+            count = 1
+            while stack and stack[-1][0] >= arr[i]:
+                count += stack.pop()[1]
+            right[i] = count
+            stack.append((arr[i], count))
+     
+       
+         ans = 0
+         for i in range(n):
+            mod = 10**9 + 7
+            ans = (ans + arr[i] * left[i] * right[i]) % mod
+     
+         return ans
+s=Solution()
+print(s.sumSubarrayMins([5,4,3]))
