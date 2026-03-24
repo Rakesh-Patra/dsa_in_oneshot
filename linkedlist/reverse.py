ddbf03b573d86867abcc8ba1a4160ece53b55a29
Rakesh Ku Patra <rakeshkupatra3@gmail.com>
2026-03-25 00:34:18 +0530

    reverse of a linkedlist program completed successfully

---
+++ linkedlist/reverse.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        while curr !=None:
+            nxt = curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+
+        return prev
